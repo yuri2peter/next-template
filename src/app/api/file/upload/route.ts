8c604710471359ebd76d0d44f479722c664e29be
yuri2 <yuri2peter@qq.com>
2024-11-04 18:24:27 -0800

    better upload (stream write)

--- src/app/api/file/upload/route.ts
+++ src/app/api/file/upload/route.ts
@@ -1,27 +1,72 @@
 import { NextRequest, NextResponse } from 'next/server';
 import path from 'path';
 import fs from 'fs-extra';
+import busboy from 'busboy';
 import { shortId } from '@/lib/string';
 import { IS_DEV_SERVER } from '@/lib/server';
+import { ReadableStream } from 'stream/web';
+import { Readable } from 'stream';
 
+// upload file( only one file at a time )
 export async function POST(req: NextRequest) {
-  if (!IS_DEV_SERVER) {
-    return NextResponse.json({
-      error: 'Not allowed in production mode',
-    });
+  if (!req.body) {
+    return NextResponse.json(
+      {
+        error: 'No body',
+      },
+      { status: 400 }
+    );
   }
-  const formData = await req.formData();
-  const file = formData.get('file') as File;
-  const fileBuffer = Buffer.from(await file.arrayBuffer());
-  const filename = file.name;
-  const extname = path.extname(filename);
-  const basename = path.basename(filename, extname);
-  const newFilename = `${basename}-${shortId()}${extname}`;
-  const filePath = path.join(process.cwd(), 'public/uploads', newFilename);
-  fs.ensureFileSync(filePath);
-  await fs.writeFile(filePath, fileBuffer);
-  return NextResponse.json({
-    filename: newFilename,
-    url: `/uploads/${newFilename}`,
+  if (!IS_DEV_SERVER) {
+    return NextResponse.json(
+      {
+        error: 'Not allowed in production mode',
+      },
+      { status: 403 }
+    );
+  }
+  const bb = busboy({
+    headers: Object.fromEntries(req.headers),
+  });
+
+  const result: {
+    filename: string;
+    url: string;
+  } = {
+    filename: '',
+    url: '',
+  };
+
+  bb.on('file', (name, file, info) => {
+    const filename = info.filename;
+    const extname = path.extname(filename);
+    const basename = path.basename(filename, extname);
+    const newFilename = `${basename}-${shortId()}${extname}`;
+    const filePath = path.join(process.cwd(), 'public/uploads', newFilename);
+    if (!result.filename) {
+      fs.ensureFileSync(filePath);
+      result.filename = newFilename;
+      result.url = `/uploads/${newFilename}`;
+      file.pipe(fs.createWriteStream(filePath));
+    }
+  });
+
+  Readable.fromWeb(req.body as ReadableStream).pipe(bb); // .fromWeb() method
+
+  return new Promise((resolve) => {
+    bb.on('close', () => {
+      if (!result.filename) {
+        resolve(
+          NextResponse.json(
+            {
+              error: 'No file uploaded',
+            },
+            { status: 400 }
+          )
+        );
+      } else {
+        resolve(NextResponse.json(result));
+      }
+    });
   });
 }
