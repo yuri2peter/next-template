40562c991b688da67eee30bcd067e601711407b2
yuri2 <yuri2peter@qq.com>
2024-11-13 11:48:20 +0800

    use grouped route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,12 +18,12 @@ dayjs.extend(isLeapYear);
 // dayjs.locale('zh-cn');
 
 const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
+  src: '../fonts/GeistVF.woff',
   variable: '--font-geist-sans',
   weight: '100 900',
 });
 const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
+  src: '../fonts/GeistMonoVF.woff',
   variable: '--font-geist-mono',
   weight: '100 900',
 });
